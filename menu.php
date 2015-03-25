ba5a1dd66774149548bb0858bb26b4e92b6a2dec
malmazan <maralva92@gmail.com>
2015-03-25 10:33:44 +0100

    Inicializar la pagina web

---
+++ menu.php
@@ -0,0 +1,24 @@
+
+<div class="container-fluid">
+<div class="navbar-header">
+    <button aria-controls="navbar" aria-expanded="false" data-target="#navbar" data-toggle="collapse" class="navbar-toggle collapsed" type="button">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand"><img id="ProjectName"src="img/Hackathon-logo.png" class="img-responsive" alt="Responsive image" /></a>
+</div>
+	<!--Nav links-->
+<div class="navbar-collapse collapse" id="navbar">
+	<ul class="nav navbar-nav" id="myTab">
+	  <li class="active"><a href="#home" aria-controls="home">Home</a></li>
+	  <li><a href="#profile" aria-controls="profile">Profile</a></li>
+	  <li><a href="#messages" aria-controls="messages">Messages</a></li>
+	  <li><a href="#settings" aria-controls="settings">Settings</a></li>
+	</ul>
+</div>
+</div> 
+
+
+
